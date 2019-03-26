1e2e4085a678a9e48dd3e08221caf3f3baed9db4
qqdipps <sdipp007@gmail.com>
2019-03-26 01:16:03 -0700

    consumed and reassinged numbers with solution, can try to do with out but does not seem part of question

--- lib/digit_match.rb
+++ lib/digit_match.rb
@@ -2,5 +2,13 @@
 # Time complexity: ?
 # Space complexity: ?
 def digit_match(number1, number2)
-  raise NotImplementedError
+  count = 0
+  until !number1 || !number2 || number1 == 0 || number2 == 0
+    count += 1 if number1 % 10 == number2 % 10
+    number1 /= 10
+    number2 /= 10
+  end
+  return count
 end
+
+# look at each last number and see if they are the same
