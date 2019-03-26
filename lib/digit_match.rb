63e9d0eac2b9f136516b6d6acb17157f74bb5813
qqdipps <sdipp007@gmail.com>
2019-03-26 01:17:33 -0700

    time complexity

--- lib/digit_match.rb
+++ lib/digit_match.rb
@@ -1,6 +1,6 @@
 # Returns count of digits matching in the two input non-negative integers
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(n) where n in the number of base-10 place values
+# Space complexity: O(1)
 def digit_match(number1, number2)
   count = 0
   until !number1 || !number2 || number1 == 0 || number2 == 0
